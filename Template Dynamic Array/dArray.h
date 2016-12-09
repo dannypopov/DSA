a116f9b5dcb17c68af3a352a7dccc28cbd5fd1ec
Unknown <jlpopov13@gmail.com>
2016-12-09 20:21:50 +0200

    Dynamic Array implementation

---
+++ Template Dynamic Array/dArray.h
@@ -0,0 +1,162 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+template<typename type>
+class dArray
+{
+private:
+	type* m_array;
+	int m_index;
+	int m_capacity;
+	void unleashData();
+	void copyData(const dArray&);
+	void resize(int);
+	void init();
+public:
+	//
+	dArray<type>();
+	~dArray<type>();
+	dArray<type>(const dArray<type>&);
+	dArray<type>& operator=(const dArray<type>&);
+	//
+	int getCapacity()const;
+	int getIndex()const;
+	type& operator[](int);
+	const type& operator[](int)const;
+	void add(type);
+};
+template<typename type>
+std::ostream& operator<<(std::ostream&, const dArray<type>&);
+
+template<typename type>
+inline int dArray<type>::getCapacity() const
+{
+	return m_capacity;
+}
+
+template<typename type>
+inline int dArray<type>::getIndex() const
+{
+	return m_index;
+}
+
+template<typename type>
+inline type & dArray<type>::operator[](int index)
+{
+	if (index >= m_capacity)
+	{
+		throw std::out_of_range("Invalid index");
+	}
+	return m_array[index];
+}
+
+template<typename type>
+inline const type & dArray<type>::operator[](int index) const
+{
+	if (index >= m_capacity)
+	{
+		throw std::out_of_range("Invalid index");
+	}
+	return m_array[index];
+}
+
+template<typename type>
+inline void dArray<type>::add(type newEl)
+{
+	if (m_index >= m_capacity)
+	{
+		int newSize = ((m_capacity == 0) ? 2 : m_capacity * 2);
+		resize(newSize);
+	}
+	m_array[m_index] = newEl;
+	m_index++;
+}
+
+template<typename type>
+inline void dArray<type>::unleashData()
+{
+	delete[] m_array;
+	m_array = nullptr;
+	m_capacity = 0;
+	m_index = 0;
+}
+
+template<typename type>
+inline void dArray<type>::copyData(const dArray &other)
+{
+	m_array = new type[other.getCapacity()];
+	for (int i = 0; i < other.getIndex(); ++i)
+	{
+		m_array = other->m_array[i];
+	}
+	m_capacity = other.getCapacity();
+	m_index = other.getIndex();
+}
+
+template<typename type>
+inline void dArray<type>::resize(int newSize)
+{
+	type* newArray = new type[newSize];
+	int elementsToCopy;
+	if (newSize < m_capacity)
+	{
+		elementsToCopy = newSize;
+	}
+	else
+	{
+		elementsToCopy = m_capacity;
+	}
+	for (int i = 0; i < elementsToCopy; ++i)
+	{
+		newArray[i] = m_array[i];
+	}
+	delete[] m_array;
+	m_array = newArray;
+	m_capacity = newSize;
+}
+template<typename type>
+inline void dArray<type>::init()
+{
+	m_capacity = 1;
+	m_index = 0;
+	m_array = new type[m_capacity];
+}
+template<typename type>
+inline dArray<type>::dArray()
+{	
+	init();
+}
+
+template<typename type>
+inline dArray<type>::~dArray()
+{
+	unleashData();
+}
+
+template<typename type>
+inline dArray<type>::dArray(const dArray &other)
+{
+	copyData(other);
+}
+
+template<typename type>
+inline dArray<type> & dArray<type>::operator=(const dArray<type> &rhs)
+{
+	if (this != &rhs)
+	{
+		unleashData();
+		copyData(rhs);
+	}
+	return *this;
+}
+template<typename type>
+inline std::ostream & operator<<(std::ostream &out, const dArray<type>&arr)
+{
+	for (int i = 0; i < arr.getIndex(); ++i)
+	{
+		out << arr[i];
+		out << "\n";
+	}
+	out << "\n";
+	return out;
+}
+#endif // if DYNAMICARRAY_H
